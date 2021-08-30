1854735d26c79a398acce09e5ff9096670e4add5
Charlotte Clark <charlocc@usc.edu>
2021-08-29 14:23:00 -1000

    Added 5 day forecast feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,6 +49,43 @@ function getCurrentWeather(city) {
         dailyWind.textContent="Wind Speed = " + data.wind.speed + " mph";
         dailyHumidity.textContent="Humidity = " + data.main.humidity + "%";
     }) 
+    function getFiveDay(lat, lon) {
+      var fiveDayUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=87c52a24cb3270fe385fbd5306e97665`
+      console.log(fiveDayUrl);
+      
+      fetch(fiveDayUrl)
+        .then(function (response) {;
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          for (var i=0; i<5; i++) {
+            var container = document.createElement('div');
+            var date = document.createElement("h3");
+            var temp = document.createElement("p");
+            var wind = document.createElement("p");
+            var humidity = document.createElement("p");
+          // Set the text content to the new variables
+            // date.textContent=data.results[i].date;
+            temp.textContent="Temperature: " + ((data.daily[i].temp.day - 273)*(9/5)+32) + " degrees Fahrenheit";
+            wind.textContent="Wind speed: " + data.daily[i].wind_speed + " mph";
+            humidity.textContent="Humidity: " + data.daily[i].humidity + "%";
+          // Add new variables to the div
+            // container.appendChild(date);
+            container.appendChild(temp);
+            container.appendChild(wind);
+            container.appendChild(humidity);
+            fiveDaysEl.appendChild(container);
+          // Style of the cards that appear
+            container.style.border = "black";
+            container.style.backgroundColor= "white";  
+            container.style.margin = "5px";
+            container.style.color = "black";
+            container.style.textAlign = "center";
+            container.style.justifyContent = "center";
+          }
+        });
+    }
 }   
 
 
@@ -56,42 +93,7 @@ function getCurrentWeather(city) {
 
 
 
-function getFiveDay(lat, lon) {
-    var fiveDayUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=87c52a24cb3270fe385fbd5306e97665`
-    console.log(fiveDayUrl);
 
-    fetch(fiveDayUrl)
-      .then(function (response) {;
-        return response.json();
-      })
-      .then(function (data) {
-//         console.log(data.____);
-//         for (var i=0; i<data.____.length; i++) {
-//           var container = document.createElement('div');
-//           var date = document.createElement("h3");
-//           var temp = document.createElement("p");
-//           var wind = document.createElement("p");
-//           var humidity = document.createElement("p");
-//         // Set the text content to the new variables
-//           date.textContent=data.results[i].date;
-//           temp.textContent=((data.results[i].temp - 273)*(9/5)+32) + " degrees Fahrenheit";
-//           wind.textContent=data.results[i].wind_speed + " mph";
-//           humidity.textContent=data.results[i].humidity;
-//         // Add new variables to the div
-//           container.appendChild(date);
-//           container.appendChild(temp);
-//           container.appendChild(wind);
-//           container.appendChild(humidity);
-//           fiveDaysEl.appendChild(container);
-//         // Style of the cards that appear
-//           container.style.border = "black";
-//           container.style.backgroundColor= "white";  
-//           container.style.margin = "5px";
-//           container.style.color = "black";
-//           container.style.textAlign = "center";
-        // }
-      });
-  }
   
 
   
